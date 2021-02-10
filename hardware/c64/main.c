35db9ebdb44f73568d35b1d9853955f08f669eac
Michal Sobczak <michalasobczak@gmail.com>
2021-02-10 14:05:25 +0100

    write and read examples

--- hardware/c64/main.c
+++ hardware/c64/main.c
@@ -1,167 +1,105 @@
 #include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h >
 
-char data1[12] = "Test Data 1";
-char data2[12] = "Test Data 2";
-char data3[12] = "\0";
-char data4[12] = "\0";
+// uninitialized section
+char* read_buffer[1000];
+static char filename[10];
+static char buffer[5];
 
-void std_write(unsigned char * file_name);
-void std_read(unsigned char * file_name);
-void kernel_read(unsigned char * file_name);
-void kernel_getin(void *ptr, unsigned char size);
+// data with defaults
+unsigned char i=0;
+unsigned char j=0;
+unsigned char tmp=0;
 
-void main(void)
-    {
-        clrscr();
+// prototypes
+FILE* open_file(unsigned char* file_name, const char* mode);
+void write_string(FILE* file, unsigned char* str);
+void read_string(FILE* file, int size);
+void close_file(FILE* file);
 
-        // File read & write with stdio functions
-        cputs("Writing with stido.h\n\r");
-        std_write("testfile");
-
-        cputs("\n\rReading with stido.h\n\r");
-        std_read("testfile");
-
-        cprintf("\n\rdata3 : %s\n\r",data3);
-        cprintf("data4 : %s\n\r",data4);
-        cprintf("\n\r");
-
-        cgetc();
-        clrscr();
-
-        // Trying to read with cbm specific functions
-        data3[0]='\0';
-        data4[0]='\0';
-
-        cputs("\n\rReading with cbm.h\n\r");
-        kernel_read("testfile");
-
-        cprintf("\n\rdata3 : %s\n\r",data3);
-        cprintf("data4 : %s\n\r",data4);
-        cprintf("\n\r");
-
-        // Trying to read non exiting file
-        data3[0]='\0';
-        data4[0]='\0';
-
-        cputs("\n\rReading with cbm.h again\n\r");
-        kernel_read("testfile2");
-
-        cprintf("\n\rdata3 : %s\n\r",data3);
-        cprintf("data4 : %s\n\r",data4);
-        cprintf("\n\r");
-
-    }
+// fastcalls
+char* __fastcall__ strcat (char* s1, const char* s2);
+char* __fastcall__ strcpy (char* s1, const char* s2);
+void __fastcall__ srand (unsigned seed);
+void __fastcall__ sleep (unsigned seconds);
 
 
-void std_write(unsigned char * file_name)
-    {
-        FILE *file;
-        unsigned char n;
+// program
+int main(void) {
+    FILE* file;    
+	srand(10);
+	clrscr();
+	for (i=0; i<=0; i++) {
+		//strcat(filename, "myfile");
+		strcpy(filename, itoa(i, buffer, 10));
+        file = open_file(filename, "w");
+        write_string(file, "testujemy");
+        close_file(file);
+		cprintf("\n\r");
+		sleep(1);
+        file = open_file(filename, "r");
+        read_string(file, 9);
+        close_file(file);
+        cputs((const char*)read_buffer);
+		//clrscr();
+	}
+	return 0;
+}
 
-        cputs("Opening data file...\n\r");
-        _filetype = 's';
-        if(file = fopen(file_name, "w"))
-            {
-                cputs("Writing...\n\r");
-                n = fwrite(data1, sizeof(unsigned char)*11, 1, file);
-                n = n + fwrite(data2, sizeof(unsigned char)*11, 1, file);
 
-                if(n != 2)
-                {
-                    cputs("Error: File could not be written.\n\r");
-                    fclose(file);
-                }
-                else
-                {
-                    cputs("Done.\n\r");
-                    fclose(file);
-                }
-            }
-        else
-            {
-                cputs("File could not be opened\n\r");
-            }
-    }
+int dummy(a)
+     int a;
+{
+    return a;
+}
 
-void std_read(unsigned char * file_name)
-    {
-        FILE *file;
-        unsigned char n;
 
-        cputs("Opening data file...\n\r");
-        _filetype = 's';
-        if(file = fopen(file_name, "r"))
-        {
-            cputs("Reading...\n\r");
-            n = fread(data3, sizeof(unsigned char)*11, 1, file);
-            n = n + fread(data4, sizeof(unsigned char)*11, 1, file);
+FILE* open_file(unsigned char* file_name, const char* mode) {
+	FILE* file;
+	cputs("open_file = 1. Opening data file...\n\r");
+    sprintf("file_name: %s - with mode - %s\r\n", file_name, mode);
+	_filetype = 's';
+	if (file = fopen(file_name, mode)) {
+       cputs("open_file = 2. File opened\n\r");
+  	} else {
+		cputs("open_file = 2. File could not be opened\n\r");
+	}
+    return file;
+}
 
-            if(n != 2)
-            {
-                cputs("Error while reading!\n\r");
-                fclose(file);
-            }
-            else
-            {
-                cprintf("Done.\n\r");
-                fclose(file);
-            }
-        }
-        else
-        {
-            cputs("File could not be opened\n\r");
-        }
-    }
 
-void kernel_read(unsigned char * file_name)
-    {
-        unsigned char error_channel;
+void write_string(FILE* file, unsigned char* str) {
+    unsigned char n = 0;
+    cputs("write_string\n\r");
+    n = fwrite(str, strlen(str), 1, file);
+	if(n != 1) {
+		cputs("write_string = error: File could not be written\n\r");
+		fclose(file);
+	} else {
+		cputs("write_string = done\n\r");
+		fclose(file);
+	}
+}
 
-        // Open error channel
-        cbm_k_setlfs(15,8,15);
-        cbm_k_setnam('\0');
-        error_channel = cbm_k_open();
 
-        cbm_k_setlfs(2,8,0);
-        cbm_k_setnam(file_name);
+void read_string(FILE* file, int size) {
+    unsigned char n = 0;
+    cputs("read_string\n\r");
+    n = fread(read_buffer, sizeof(unsigned char)*size, 1, file);
+	if(n != 1) {
+		cputs("read_string = error: File could not be read\n\r");
+		fclose(file);
+	} else {
+		cputs("read_string = done\n\r");
+		fclose(file);
+	}    
+}
 
-        cputs("Opening data file...\n\r");
-        if(!cbm_k_open())
-        {
-            _filetype = 's';
-            cputs("Reading...\n\r");
-            cbm_k_chkin(2);
-            kernel_getin(data3, sizeof(unsigned char)*11);
-            kernel_getin(data4, sizeof(unsigned char)*11);
 
-            cbm_k_chkin(15);
-            if(!cbm_k_getin())
-            {
-                cputs("Error while reading!\n\r");
-                cbm_k_clall();
-            }
-            else
-            {
-                cprintf("Done.\n\r");
-                cbm_k_clall();
+void close_file(FILE* file) {
+    cputs("close_file\n\r");
+    fclose(file);
+}
 
-            }
-        }
-        else
-        {
-            cputs("File could not be opened\n\r");
-        }
-    }
-
-void kernel_getin(void *ptr, unsigned char size)
-    {
-        unsigned char * data = (unsigned char *)ptr;
-        unsigned char i;
-
-        for(i=0; i<size; ++i)
-        {
-            data[i] = cbm_k_getin();
-        }
-        data[i] = '\0';
-    }
