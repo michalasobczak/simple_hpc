bd21ade6fb723a688b07d13853404eb0fbf267e4
Michal Sobczak <michalasobczak@gmail.com>
2021-02-19 23:10:25 +0100

    format c64 c example

--- hardware/c64/main.c
+++ hardware/c64/main.c
@@ -28,21 +28,21 @@ void __fastcall__ sleep (unsigned seconds);
 
 // program
 int main(void) {
-    FILE* file;    
+	FILE* file;
 	srand(10);
 	clrscr();
 	for (i=0; i<=0; i++) {
 		//strcat(filename, "myfile");
 		strcpy(filename, itoa(i, buffer, 10));
-        file = open_file(filename, "w");
-        write_string(file, "testujemy");
-        close_file(file);
+		file = open_file(filename, "w");
+		write_string(file, "testujemy");
+		close_file(file);
 		cprintf("\n\r");
 		sleep(1);
-        file = open_file(filename, "r");
-        read_string(file, 9);
-        close_file(file);
-        cputs((const char*)read_buffer);
+		file = open_file(filename, "r");
+		read_string(file, 9);
+		close_file(file);
+		cputs((const char*)read_buffer);
 		//clrscr();
 	}
 	return 0;
@@ -50,30 +50,30 @@ int main(void) {
 
 
 int dummy(a)
-     int a;
+int a;
 {
-    return a;
+	return a;
 }
 
 
 FILE* open_file(unsigned char* file_name, const char* mode) {
 	FILE* file;
 	cputs("open_file = 1. Opening data file...\n\r");
-    sprintf("file_name: %s - with mode - %s\r\n", file_name, mode);
+	sprintf("file_name: %s - with mode - %s\r\n", file_name, mode);
 	_filetype = 's';
 	if (file = fopen(file_name, mode)) {
-       cputs("open_file = 2. File opened\n\r");
-  	} else {
+		cputs("open_file = 2. File opened\n\r");
+	} else {
 		cputs("open_file = 2. File could not be opened\n\r");
 	}
-    return file;
+	return file;
 }
 
 
 void write_string(FILE* file, unsigned char* str) {
-    unsigned char n = 0;
-    cputs("write_string\n\r");
-    n = fwrite(str, strlen(str), 1, file);
+	unsigned char n = 0;
+	cputs("write_string\n\r");
+	n = fwrite(str, strlen(str), 1, file);
 	if(n != 1) {
 		cputs("write_string = error: File could not be written\n\r");
 		fclose(file);
@@ -85,21 +85,21 @@ void write_string(FILE* file, unsigned char* str) {
 
 
 void read_string(FILE* file, int size) {
-    unsigned char n = 0;
-    cputs("read_string\n\r");
-    n = fread(read_buffer, sizeof(unsigned char)*size, 1, file);
+	unsigned char n = 0;
+	cputs("read_string\n\r");
+	n = fread(read_buffer, sizeof(unsigned char)*size, 1, file);
 	if(n != 1) {
 		cputs("read_string = error: File could not be read\n\r");
 		fclose(file);
 	} else {
 		cputs("read_string = done\n\r");
 		fclose(file);
-	}    
+	}
 }
 
 
 void close_file(FILE* file) {
-    cputs("close_file\n\r");
-    fclose(file);
+	cputs("close_file\n\r");
+	fclose(file);
 }
 
