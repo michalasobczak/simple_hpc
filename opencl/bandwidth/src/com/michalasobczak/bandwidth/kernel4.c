c539e1d9729bb342e59f65dc9060d06dce5d2aa2
Michal Sobczak <michalasobczak@gmail.com>
2021-10-22 21:03:43 +0200

    kernel4

--- opencl/bandwidth/src/com/michalasobczak/bandwidth/kernel4.c
+++ opencl/bandwidth/src/com/michalasobczak/bandwidth/kernel4.c
@@ -6,9 +6,10 @@ __kernel void sampleKernel(__global const float *a,
     __private int gc  = get_num_groups(0);
     __private int lid = get_local_id(0);
     __private int ls  = get_local_size(0);
+    //
     __private int eid = (gr*ls)+lid;
     __private int gs  = gc*ls;
-
-    //printf(" %i \n", gid);
-    d[gid] = ls;
+    //
+    printf(" %i \n", gid);
+    d[gid] = gs;
 }
