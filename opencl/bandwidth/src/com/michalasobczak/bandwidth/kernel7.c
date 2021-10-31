712039d14ae1975627838111b4375f232df04c8b
Michal Sobczak <michalasobczak@gmail.com>
2021-10-31 10:34:05 +0100

    bandwidth / kernel7, kernel8

--- opencl/bandwidth/src/com/michalasobczak/bandwidth/kernel7.c
+++ opencl/bandwidth/src/com/michalasobczak/bandwidth/kernel7.c
@@ -2,9 +2,7 @@ __kernel void sampleKernel(__global const float *a,
                            __global float *d)
 {
     __private int gid = get_global_id(0);
-    int j = 0;
-    while (1) {
-      j = j + 1;
-    }
-    d[gid] = j;
+    __private int group_id = get_group_id(0);
+    d[group_id] = d[group_id] + 1;
+    printf("%u %u %f \n", group_id, gid, d[group_id]);
 }
---
+++ opencl/bandwidth/src/com/michalasobczak/bandwidth/kernel8.c
@@ -0,0 +1,12 @@
+__kernel void sampleKernel(__global const float *a,
+                           __global float *d)
+{
+    __private int gid = get_global_id(0);
+    __private int group_id = get_group_id(0);
+    __local int shared;
+    shared = 0.0;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    atomic_inc(&shared);
+    d[group_id] = shared;
+    printf("%u %u %f %i\n", group_id, gid, d[group_id], shared);
+}
