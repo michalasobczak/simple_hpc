91c2d9ab9c1ee2dfa85a25cab3c6affc853a801f
Michal Sobczak <michalasobczak@gmail.com>
2021-11-25 21:29:05 +0100

    reduction first test

---
+++ opencl/reduction/src/com/michalasobczak/reduction/Main.java
@@ -0,0 +1,70 @@
+package com.michalasobczak.reduction;
+
+import com.michalasobczak.opencl.PlatformParametersSet;
+import com.michalasobczak.opencl.RuntimeConfigurationSet;
+import com.michalasobczak.opencl.Utils;
+import org.jocl.CL;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Utils.log("***** REDUCTION                     *****");
+        Utils.log("***** com.michalasobczak.reduction  *****");
+
+        // -----
+        Utils.log("0. OpenCL specific configuration");
+        Utils.log(" - Enable exceptions and subsequently omit error checks in this sample");
+        CL.setExceptionsEnabled(true);
+
+        // -----
+        Utils.log("1. Initialize configuration classes");
+        int n = 1024*1024*64;
+        int loc = 1024;
+        PlatformParametersSet p = new PlatformParametersSet();
+        RuntimeConfigurationSet r = new RuntimeConfigurationSet();
+        KernelConfigurationSet c = new KernelConfigurationSet(n, loc);
+
+        // -----
+        Utils.log("2. Get and print platform/devices parameters");
+        p.getPlatformsAndDevices();
+        p.getDevicesInfo();
+
+        // -----
+        Utils.log("3. Platform and device selection");
+        r.selectPlatform(1);
+        r.selectDevice(1);
+
+        // -----
+        Utils.log("4. Create input and output data");
+        c.initializeSrcArrayA();
+        c.initializeDstArray();
+        c.generateSampleRandomData();
+        c.printSrcArray();
+
+        // -----
+        Utils.log("4. Create context and command queue and buffers");
+        c.createContext();
+        c.createCommandQueue();
+        c.createBuffers();
+
+        // -----
+        Utils.log("5. Read kernel file, create program, pass arguments");
+        c.readKernelFile();
+        c.initializeKernel();
+        c.configureWork();
+
+        // -----
+        Utils.log("6. Run kernel, read buffer");
+        c.runKernel(1);
+
+        // -----
+        Utils.log("7. Release kernel, program, and memory objects");
+        c.releaseResources();
+
+        // -----
+        Utils.log("8. Debug results");
+        c.printResults();
+    } // main
+
+} // class Main
--- opencl/reduction/src/com/michalasobczak/reduction/kernel.c
+++ opencl/reduction/src/com/michalasobczak/reduction/kernel.c
@@ -33,33 +33,18 @@ __kernel void sampleKernel(__global const float *a, __global float *d, __local f
     __private int gid = get_global_id(0);
     __private int lid = get_local_id(0);
     __private int group_id = get_group_id(0);
-
-    int power_count = find_power_of_2(gs);
-
-    //printf("%u, %u\n", gc, ls);
-    //printf("%u\n", power_count);
-
     partial_sums[lid] = a[gid];
-
     barrier(CLK_LOCAL_MEM_FENCE);
-
-    //__local float sub[LOCAL];
-    //sub[0] = a[(group_id*LOCAL)+0];
-    //sub[1] = a[(group_id*LOCAL)+1];
-    //sort_local(sub);
-    //printf("1.  %u, %f %f\n", gid, sub[0], sub[1]);
-
-    //for (int i=0; i<power_count; i++) {
-    for (int i=gs/2; i>0; i >>= 1) {
+    for (int i=ls/2; i>0; i >>= 1) {
       //preview_output(d);
       if (lid < i) {
-        //printf("step %u -> %u %u %f\n", i, gid, lid, a[gid]);
+        //printf("step %u -> %u %u %f -> %u\n", i, gid, lid, a[gid], group_id);
         partial_sums[lid] += partial_sums[lid + i];
       }
       barrier(CLK_LOCAL_MEM_FENCE);
     }
-
     if (lid == 0) {
       d[group_id] = partial_sums[0];
+      //printf("d[group_id] = %f, lid=%u, gid=%u, group=%u\n", d[group_id], lid, gid, group_id);
     }
 }
