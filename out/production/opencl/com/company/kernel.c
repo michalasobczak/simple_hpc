1a66147c566d8df8201f4bc1f2bbb917e9d1fbef
Michal Sobczak <michalasobczak@gmail.com>
2022-01-10 21:53:43 +0100

    Removing out/production

--- out/production/opencl/com/company/kernel.c
+++
@@ -1,9 +0,0 @@
-
-__kernel void sampleKernel(__global const float *a,
-                           __global float *d)
-{
-    __private int gid = get_global_id(0);
-    //__private int lid = get_local_id(0);
-    d[gid] = (sin(a[gid]) + cos(a[gid]) * 3.14);
-    //d[gid] = lid;
-}
--- out/production/reusing_code/com/company2/kernel.c
+++
@@ -1,8 +0,0 @@
-
-__kernel void sampleKernel(__global const float *a,
-                           __global float *d)
-{
-    __private int gid = get_global_id(0);
-    __private int lid = get_local_id(0);
-    d[gid] = lid;
-}
